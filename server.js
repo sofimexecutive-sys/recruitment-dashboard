344fc743c447eed9c2ba155927bafdd0b9e9ff02
sofimexecutive-sys <sofim.executive@gmail.com>
2026-06-22 17:06:44 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'recruitment_dashboard.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Dashboard running on port ${PORT}`);
+});
